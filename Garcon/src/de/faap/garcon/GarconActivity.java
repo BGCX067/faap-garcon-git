e51afc2249c6ced3e40d57f56c007fe2d2d98ca4
Alexander Aumann <a.aumann@web.de>
2012-03-23 21:18:28 +0100

    Initial commit Containing a quasi-empty android project, with .gitignore file, the support v4 library and proguard.cfg as suggested by the SherlockActionBar team. Sherlock not included here.

---
+++ Garcon/src/de/faap/garcon/GarconActivity.java
@@ -0,0 +1,13 @@
+package de.faap.garcon;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GarconActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
