59d42edf50e4332b227b2028e54d229a2f1114c3
faltermj <johannes.faltermeier@gmail.com>
2012-03-27 00:05:34 +0200

    Dashboard ActionItems
    - basic functionality of action items now implemented

--- Garcon/src/de/faap/garcon/ui/DashboardActivity.java
+++ Garcon/src/de/faap/garcon/ui/DashboardActivity.java
@@ -2,12 +2,19 @@ package de.faap.garcon.ui;
 
 import android.os.*;
 import android.view.*;
+import android.view.inputmethod.*;
+import android.widget.*;
 import com.actionbarsherlock.app.*;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 import de.faap.garcon.*;
 
 public class DashboardActivity extends SherlockActivity {
+    // keep a handle to MenuItems which have an ActionView to collapse them when
+    // needed
+    protected MenuItem searchRestaurant;
+    protected MenuItem searchDish;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -88,6 +95,61 @@ public class DashboardActivity extends SherlockActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getSupportMenuInflater().inflate(R.menu.dashboard_menu_items, menu);
+
+        // If we're on a phone with at least honeycomb we use the searchview
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            // configure search restaurant
+            searchRestaurant = menu.findItem(R.id.menu_search_restaurant);
+            SearchView restaurantSearchView =
+                    (SearchView) searchRestaurant.getActionView();
+            restaurantSearchView.setQueryHint(getResources()
+                    .getString(R.string.menu_search_restaurant_hint));
+            restaurantSearchView.setImeOptions(EditorInfo.IME_ACTION_SEARCH);
+            restaurantSearchView
+                    .setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+                        @Override
+                        public boolean onQueryTextChange(String newText) {
+                            return false;
+                        }
+
+                        @Override
+                        public boolean onQueryTextSubmit(String query) {
+                            // TODO fire intent
+                            setTitle(query);
+                            searchRestaurant.collapseActionView();
+                            return false;
+                        }
+
+                    });
+
+            // configure search dish
+            searchDish = menu.findItem(R.id.menu_search_dish);
+            SearchView dishSearchView = (SearchView) searchDish.getActionView();
+            dishSearchView.setQueryHint(getResources()
+                    .getString(R.string.menu_search_dish_hint));
+            restaurantSearchView.setImeOptions(EditorInfo.IME_ACTION_SEARCH);
+            dishSearchView
+                    .setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+                        @Override
+                        public boolean onQueryTextChange(String newText) {
+                            return true;
+                        }
+
+                        @Override
+                        public boolean onQueryTextSubmit(String query) {
+                            // TODO fire intent
+                            setTitle(query);
+                            // TODO check if needed, when we come back from new
+                            // activity
+                            searchDish.collapseActionView();
+                            return true;
+                        }
+
+                    });
+        }
+
         return true;
     }
 
@@ -95,22 +157,46 @@ public class DashboardActivity extends SherlockActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
         case R.id.menu_nearby_restaurants:
+            // TODO add behaviour
             setTitle(getResources().getString(R.string.btn_nearby_restaurants));
             return true;
 
         case R.id.menu_nearby_dishes:
+            // TODO add behaviour
             setTitle(getResources().getString(R.string.btn_nearby_dishes));
             return true;
 
+        case R.id.menu_search:
+            // before starting a new search hide old search views
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                searchRestaurant.collapseActionView();
+                searchDish.collapseActionView();
+            }
+            return true;
+
         case R.id.menu_search_restaurant:
+            // if we are on a pre-honeycomb device we can't start a searchview,
+            // so we start the new activity with an empty search request
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+                // TODO fire intent
+                setTitle("leere suche");
+            }
             return true;
 
         case R.id.menu_search_dish:
+            // if we are on a pre-honeycomb device we can't start a searchview,
+            // so we start the new activity with an empty search request
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+                // TODO fire intent
+                setTitle("leere suche");
+            }
             return true;
 
         case R.id.menu_post_dish:
+            // TODO add behaviour
             setTitle(getResources().getString(R.string.btn_post_dish));
             return true;
+
         default:
             return super.onOptionsItemSelected(item);
         }
