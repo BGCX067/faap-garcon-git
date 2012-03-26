4be801b2de417d689dc497b5afbf588bb621bc83
faltermj <johannes.faltermeier@gmail.com>
2012-03-26 21:03:59 +0200

    DashboardActivity ActionBar further implemented
    - Home button not clickable
    - ActionItems added
    - SearchView for newer phones added

--- Garcon/src/de/faap/garcon/ui/DashboardActivity.java
+++ Garcon/src/de/faap/garcon/ui/DashboardActivity.java
@@ -3,6 +3,8 @@ package de.faap.garcon.ui;
 import android.os.*;
 import android.view.*;
 import com.actionbarsherlock.app.*;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
 import de.faap.garcon.*;
 
 public class DashboardActivity extends SherlockActivity {
@@ -11,6 +13,9 @@ public class DashboardActivity extends SherlockActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        // make the home button not clickable
+        getSupportActionBar().setHomeButtonEnabled(false);
 
         // add button listeners
         this.findViewById(R.id.home_btn_post_dish)
@@ -78,5 +83,36 @@ public class DashboardActivity extends SherlockActivity {
                     }
 
                 });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getSupportMenuInflater().inflate(R.menu.dashboard_menu_items, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.menu_nearby_restaurants:
+            setTitle(getResources().getString(R.string.btn_nearby_restaurants));
+            return true;
+
+        case R.id.menu_nearby_dishes:
+            setTitle(getResources().getString(R.string.btn_nearby_dishes));
+            return true;
+
+        case R.id.menu_search_restaurant:
+            return true;
+
+        case R.id.menu_search_dish:
+            return true;
+
+        case R.id.menu_post_dish:
+            setTitle(getResources().getString(R.string.btn_post_dish));
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
