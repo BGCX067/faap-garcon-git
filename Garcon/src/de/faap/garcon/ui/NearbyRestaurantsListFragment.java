79940c59163bb3323c69324ebf1d7e6ca547cf7a
faltermj <johannes.faltermeier@gmail.com>
2012-03-30 14:21:49 +0200

    Support Lib, NearbyRestaurants
    - removed old support lib, because of errors
    - displaying a map and a list of strings in nearbyrestaurants now

--- Garcon/src/de/faap/garcon/ui/NearbyRestaurantsListFragment.java
+++ Garcon/src/de/faap/garcon/ui/NearbyRestaurantsListFragment.java
@@ -2,16 +2,57 @@ package de.faap.garcon.ui;
 
 import android.os.*;
 import android.view.*;
+import android.widget.*;
 import com.actionbarsherlock.app.*;
 import de.faap.garcon.*;
 
 public class NearbyRestaurantsListFragment extends SherlockFragment {
+    // Tag for FragmentManager
+    public static final String TAG = "NearbyRestaurantsListFragment";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View v =
                 inflater.inflate(R.layout.fragment_nearbyrestaurantslist,
                                  container, false);
+
+        setUpMap((ViewGroup) v
+                .findViewById(R.id.nearbyrestlistfrag_map_container));
+
+        setUpList((ListView) v.findViewById(R.id.nearbyrestlistfrag_list_view));
+
         return v;
+    }
+
+    private void setUpMap(ViewGroup container) {
+        NearbyRestaurantsActivity.MapSingleton.mMapView.setClickable(true);
+        NearbyRestaurantsActivity.MapSingleton.mMapView
+                .setBuiltInZoomControls(true);
+
+        container.addView(NearbyRestaurantsActivity.MapSingleton.mMapView);
+
+        // If an exception occurs that the MapView already has a parent,
+        // uncomment code below
+
+        // final ViewGroup parent =
+        // (ViewGroup) NearbyRestaurantsActivity.MapSingleton.mMapView
+        // .getParent();
+        // if (parent != null)
+        // parent.removeView(NearbyRestaurantsActivity.MapSingleton.mMapView);
+    }
+
+    private void setUpList(ListView lv) {
+        String[] myList =
+                new String[] { "Hello", "World", "Foo", "Bar", "World", "Foo",
+                        "Bar" };
+        lv.setAdapter(new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, myList));
     }
 }
