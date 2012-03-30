f50ff924de54d217e8f52755f6badac1bacbcf9f
faltermj <johannes.faltermeier@gmail.com>
2012-03-31 00:09:36 +0200

    Location Service
    - removed unnecessary permission
    - the map centers now on the current location

--- Garcon/src/de/faap/garcon/ui/NearbyRestaurantsListFragment.java
+++ Garcon/src/de/faap/garcon/ui/NearbyRestaurantsListFragment.java
@@ -1,10 +1,14 @@
 package de.faap.garcon.ui;
 
 import java.util.*;
+import android.content.*;
+import android.location.*;
 import android.os.*;
+import android.util.*;
 import android.view.*;
 import android.widget.*;
 import com.actionbarsherlock.app.*;
+import com.google.android.maps.*;
 import de.faap.garcon.*;
 import de.faap.garcon.ui.widget.*;
 import de.faap.garcon.util.*;
@@ -13,10 +17,51 @@ public class NearbyRestaurantsListFragment extends SherlockFragment {
     // Tag for FragmentManager
     public static final String TAG = "NearbyRestaurantsListFragment";
 
+    private LocationManager locationManager;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+
+        // Acquire a reference to the system Location Manager
+        locationManager =
+                (LocationManager) getActivity()
+                        .getSystemService(Context.LOCATION_SERVICE);
+
+        // Define a listener that responds to location updates
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location
+                // provider.
+                showOnMap(location);
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status,
+                    Bundle extras) {
+                // do nada
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+                // do nada
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                // do nada
+            }
+        };
+
+        // Register the listener with the Location Manager to receive location
+        // updates
+        locationManager
+                .requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0,
+                                        locationListener);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,
+                                               0, locationListener);
     }
 
     @Override
@@ -59,5 +104,16 @@ public class NearbyRestaurantsListFragment extends SherlockFragment {
 
         lv.setAdapter(new RestaurantListAdapter(getActivity(),
                 R.layout.restaurant_list_item_position, restaurants));
+    }
+
+    private void showOnMap(Location l) {
+        Log.d("faap.garcon", "showOnMap called");
+        MapController mapController =
+                NearbyRestaurantsActivity.MapSingleton.mMapView.getController();
+        mapController.setZoom(16);
+        Double lat = l.getLatitude() * 1E6;
+        Double lng = l.getLongitude() * 1E6;
+        GeoPoint geoPoint = new GeoPoint(lat.intValue(), lng.intValue());
+        mapController.setCenter(geoPoint);
     }
 }
